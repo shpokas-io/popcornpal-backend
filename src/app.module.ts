207c1e92077b5ff80ba5d13257b745db066a7f11
shpokas <s.shpokas@gmail.com>
2024-11-04 16:26:28 +0200

    refactor: removed redundant declarations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { SupabaseService } from './supabase/supabase.service';
 import { MoviesModule } from './movies/movies.module';
 import { SupabaseModule } from './supabase/supabase.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  providers: [SupabaseService],
-  exports: [SupabaseService],
   imports: [MoviesModule, SupabaseModule, AuthModule],
 })
 export class AppModule {}
