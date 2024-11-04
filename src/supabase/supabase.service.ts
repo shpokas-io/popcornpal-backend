4b46b7883181db8f6b782304adebac492a9ed6cc
shpokas <s.shpokas@gmail.com>
2024-11-04 16:30:23 +0200

    refactor: added logger to log errors & added check if spabase_url&key are defined

--- src/supabase/supabase.service.ts
+++ src/supabase/supabase.service.ts
@@ -7,10 +7,18 @@ dotenv.config();
 @Injectable()
 export class SupabaseService {
   private supabase: SupabaseClient;
+  private readonly logger = new Logger(SupabaseService.name);
 
   constructor() {
     const SUPABASE_URL = process.env.SUPABASE_URL;
     const SUPABASE_KEY = process.env.SUPABASE_KEY;
+
+    if (!SUPABASE_URL || !SUPABASE_KEY) {
+      this.logger.error(
+        'Supabase URL or Key is missing in environment variables',
+      );
+      throw new Error('Supabase configuration error: URL or Key missing');
+    }
 
     this.supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
   }
