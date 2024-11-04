6b482b158ba00b168a700632c77774fa7e30b183
shpokas <s.shpokas@gmail.com>
2024-11-04 11:56:13 +0200

    fix: imported supabase model

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,11 @@ import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
+import { SupabaseModule } from '../supabase/supabase.module';
 
 @Module({
   imports: [
+    SupabaseModule,
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
